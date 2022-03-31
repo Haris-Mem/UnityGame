0de6dab15bfb8ca350476b41b9f3692ad55222f8
Haris-Mem <90726020+Haris-Mem@users.noreply.github.com>
2022-03-31 19:15:18 -0400

    adding quests

---
+++ Phase2_Hi/Assets/RpgAdv/Scripts/Quests/QuestManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace RpgAdventure
+{
+    public class QuestManager : MonoBehaviour
+    {
+        public List<Quest> quests;
+        
+    }
+}
