48dfe820bc5604da0ed96d991bf5386d0607d59f
Haris-Mem <90726020+Haris-Mem@users.noreply.github.com>
2022-04-02 19:06:07 -0400

    Adding Quest Log

---
+++ Phase2_Hi/Assets/RpgAdv/Scripts/Quests/QuestLog.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RpgAdv
+{
+    public enum QuestStatus
+    {
+        ACTIVE,
+        FAILED,
+        COMPLETED
+    }
+
+    [System.Serializable]
+    public class AcceptedQuest : Quest
+    {
+        public QuestStatus status;
+
+        public AcceptedQuest(Quest quest)
+        {
+            uid = quest.uid;
+            title = quest.title;
+            description = quest.description;
+            experience = quest.experience;
+            gold = quest.gold;
+            amount = quest.amount;
+            targets = quest.targets;
+            talkTo = quest.talkTo;
+            explore = quest.explore;
+            questGiver = quest.questGiver;
+            type = quest.type;
+            status = QuestStatus.ACTIVE;
+        }
+    }
+
+    public class QuestLog : MonoBehaviour
+    {
+        public List<AcceptedQuest> quests = new List<AcceptedQuest>();
+
+        public void AddQuest(Quest quest)
+        {
+            quests.Add(new AcceptedQuest(quest));
+        }
+    }
+}
