a25fbf96d550c9fb6b1045c12c0f55c50ab2979e
msothyn <90815030+msothyn@users.noreply.github.com>
2022-04-05 21:59:28 -0400

    Update HudManager.cs

--- Phase2_Hi/Assets/RpgAdv/Scripts/Hud/HudManager.cs
+++ Phase2_Hi/Assets/RpgAdv/Scripts/Hud/HudManager.cs
@@ -7,14 +7,17 @@ namespace RpgAdv { }
 public class HudManager : MonoBehaviour
 {
 
+// variable declarations
     public Slider healthSlider;
 
+// set max health
     public void SetMaxHealth(int health) {
 
         healthSlider.maxValue = health;
         SetHealth(health);
     }
 
+// set the health
     public void SetHealth(int health) {
         healthSlider.value = health;
 
