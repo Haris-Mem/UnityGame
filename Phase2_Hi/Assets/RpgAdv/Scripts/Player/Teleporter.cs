f9f47f90c9210b819e4aa5daec29e1d480784f41
msothyn <90815030+msothyn@users.noreply.github.com>
2022-04-05 22:14:33 -0400

    commenting!!!

--- Phase2_Hi/Assets/RpgAdv/Scripts/Player/Teleporter.cs
+++ Phase2_Hi/Assets/RpgAdv/Scripts/Player/Teleporter.cs
@@ -4,9 +4,11 @@ using UnityEngine;
  
 public class Teleporter : MonoBehaviour
 {
+// variable declarations 
     public GameObject Player;
     public GameObject TeleportTo;
 
+// when the player steps on the teleport pad they get teleported to the target pad.
     void OnTriggerEnter(Collider col)
     {
         Player.transform.position = TeleportTo.transform.position;
